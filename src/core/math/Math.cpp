40c1433f40a654ded86b48793d07bf88352034d1
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2020-12-29 01:05:45 +0100

    Added simle math functions

---
+++ src/core/math/Math.cpp
@@ -0,0 +1,89 @@
+#include "Math.h"
+
+#include <math.h>
+#include <algorithm>
+
+float Euler::Math::Deg(float radians)
+{
+	return radians * Rad2Deg;
+}
+
+float Euler::Math::Rad(float degrees)
+{
+	return degrees * Rad2Deg;
+}
+
+float Euler::Math::Sin(float radians)
+{
+	return sinf(radians);
+}
+
+float Euler::Math::Cos(float radians)
+{
+	return cosf(radians);
+}
+
+float Euler::Math::Tan(float radians)
+{
+	return tanf(radians);
+}
+
+float Euler::Math::Pow(float base, float exponent)
+{
+	return powf(base, exponent);
+}
+
+float Euler::Math::Sqrt(float x)
+{
+	return sqrtf(x);
+}
+
+float Euler::Math::Abs(float x)
+{
+	return abs(x);
+}
+
+int Euler::Math::Abs(int x)
+{
+	return abs(x);
+}
+
+float Euler::Math::Min(float a, float b)
+{
+	return std::min(a, b);
+}
+
+int Euler::Math::Min(int a, int b)
+{
+	return std::min(a, b);
+}
+
+float Euler::Math::Max(float a, float b)
+{
+	return std::max(a, b);
+}
+
+int Euler::Math::Max(int a, int b)
+{
+	return std::max(a, b);
+}
+
+float Euler::Math::Clamp(float value, float min, float max)
+{
+	return Max(min, Min(value, max));
+}
+
+int Euler::Math::Clamp(int value, int min, int max)
+{
+	return Max(min, Min(value, max));
+}
+
+float Euler::Math::Lerp(float a, float b, float t)
+{
+	return Clamp(LerpUnclamped(a, b, t), a, b);
+}
+
+float Euler::Math::LerpUnclamped(float a, float b, float t)
+{
+	return a * (1.0f - t) + b * t;
+}
---
+++ src/core/math/Math.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "../API.h"
+
+#define PI 3.14159265359f
+#define Rad2Deg 180.0f/PI
+#define Deg2Rad PI/180.0f
+
+namespace Euler
+{
+	namespace Math
+	{
+		/* === Angle unit conversion === */
+		float Deg(float radians);
+		float Rad(float degrees);
+
+		/* === Math function wrappers === */
+		float Sin(float radians);
+		float Cos(float radians);
+		float Tan(float radians);
+		float Pow(float base, float exponent);
+		float Sqrt(float x);
+
+		float Abs(float x);
+		int Abs(int x);
+
+		float Min(float a, float b);
+		int Min(int a, int b);
+
+		float Max(float a, float b);
+		int Max(int a, int b);
+
+		float Clamp(float value, float min, float max);
+		int Clamp(int value, int min, int max);
+
+		float Lerp(float a, float b, float t);
+		float LerpUnclamped(float a, float b, float t);
+	}
+}
