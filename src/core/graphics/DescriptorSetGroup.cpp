e1daefff98862eabf431b0c62b2b82a9cc42f00c
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2021-01-04 11:35:00 +0100

    Added DescriptorSetGroup class

---
+++ src/core/graphics/DescriptorSetGroup.cpp
@@ -0,0 +1,64 @@
+#include "DescriptorSetGroup.h"
+
+using namespace Euler::Graphics;
+
+void DescriptorSet::Allocate(Vulkan* vulkan, uint32_t count, VkDescriptorSetLayout layout, VkDescriptorPool pool)
+{
+	DescriptorSets.resize(count);
+
+	std::vector<VkDescriptorSetLayout> layouts(count, layout);
+
+	VkDescriptorSetAllocateInfo allocInfo{};
+	allocInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO;
+	allocInfo.descriptorPool = pool;
+	allocInfo.descriptorSetCount = count;
+	allocInfo.pSetLayouts = layouts.data();
+
+	// TODO: Change this with api call
+	vkAllocateDescriptorSets(vulkan->_device, &allocInfo, DescriptorSets.data());
+
+	_pool = pool;
+}
+
+void DescriptorSet::Free(Vulkan* vulkan)
+{
+	vkFreeDescriptorSets(vulkan->_device, _pool, DescriptorSets.size(), DescriptorSets.data());
+}
+
+void DescriptorSet::UpdateUniformBuffer(Vulkan* vulkan, uint32_t descriptorSetIndex, VkBuffer buffer, uint32_t dstBinding)
+{
+	VkDescriptorBufferInfo bufferInfo{};
+	bufferInfo.buffer = buffer;
+	bufferInfo.offset = 0;
+	bufferInfo.range = VK_WHOLE_SIZE;
+
+	VkWriteDescriptorSet write{};
+	write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+	write.dstSet = DescriptorSets[descriptorSetIndex];
+	write.dstBinding = dstBinding;
+	write.dstArrayElement = 0;
+	write.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+	write.descriptorCount = 1;
+	write.pBufferInfo = &bufferInfo;
+
+	vkUpdateDescriptorSets(vulkan->_device, 1, &write, 0, nullptr);
+}
+
+void DescriptorSet::UpdateUniformBufferDynamic(Vulkan* vulkan, uint32_t descriptorSetIndex, VkBuffer buffer, uint32_t dstBinding)
+{
+	VkDescriptorBufferInfo bufferInfo{};
+	bufferInfo.buffer = buffer;
+	bufferInfo.offset = 0;
+	bufferInfo.range = VK_WHOLE_SIZE;
+
+	VkWriteDescriptorSet write{};
+	write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+	write.dstSet = DescriptorSets[descriptorSetIndex];
+	write.dstBinding = dstBinding;
+	write.dstArrayElement = 0;
+	write.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC;
+	write.descriptorCount = 1;
+	write.pBufferInfo = &bufferInfo;
+
+	vkUpdateDescriptorSets(vulkan->_device, 1, &write, 0, nullptr);
+}
---
+++ src/core/graphics/DescriptorSetGroup.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "../API.h"
+#include "vulkan/Vulkan.h"
+
+#include <vector>
+#include <stdint.h>
+
+namespace Euler
+{
+	namespace Graphics
+	{
+		class EULER_API DescriptorSet
+		{
+		private:
+			VkDescriptorPool _pool;
+
+		public:
+			std::vector<VkDescriptorSet> DescriptorSets;
+
+			void Allocate(Vulkan* vulkan, uint32_t count, VkDescriptorSetLayout layout, VkDescriptorPool pool);
+			void Free(Vulkan* vulkan);
+
+			void UpdateUniformBuffer(Vulkan* vulkan, uint32_t descriptorSetIndex, VkBuffer buffer, uint32_t dstBinding);
+			void UpdateUniformBufferDynamic(Vulkan* vulkan, uint32_t descriptorSetIndex, VkBuffer buffer, uint32_t dstBinding);
+			// TODO: UpdateSampler
+		};
+	}
+};
