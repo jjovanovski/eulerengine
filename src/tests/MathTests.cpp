f79e01a5fb4c8fd9ff4951881f6636f2827ad384
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2021-01-07 14:01:35 +0100

    added tests for deg() and rad()

--- src/tests/MathTests.cpp
+++ src/tests/MathTests.cpp
@@ -31,3 +31,23 @@ TEST(MathTests, Cos) {
 	ASSERT_TRUE(AlmostEqual(Cos(3.0f * PI / 2.0f), 0.0f));
 	ASSERT_TRUE(AlmostEqual(Cos(5.0f * PI / 2.0f), -0.0f));
 }
+
+TEST(MathTests, DegToRad) {
+	ASSERT_TRUE(AlmostEqual(Rad(0.0f), 0.0f));
+	ASSERT_TRUE(AlmostEqual(Rad(45.0f), PI / 4.0f));
+	ASSERT_TRUE(AlmostEqual(Rad(90.0f), PI / 2.0f));
+	ASSERT_TRUE(AlmostEqual(Rad(180.0f), PI));
+	ASSERT_TRUE(AlmostEqual(Rad(270.0f), 3.0f * PI / 2.0f));
+	ASSERT_TRUE(AlmostEqual(Rad(360.0f), 2.0f * PI));
+	ASSERT_TRUE(AlmostEqual(Rad(-45.0f), -PI / 4.0f));
+}
+
+TEST(MathTests, RadToDeg) {
+	ASSERT_TRUE(AlmostEqual(Deg(0.0f), 0.0f));
+	ASSERT_TRUE(AlmostEqual(Deg(PI / 4.0f), 45.0f));
+	ASSERT_TRUE(AlmostEqual(Deg(PI / 2.0f), 90.0f));
+	ASSERT_TRUE(AlmostEqual(Deg(PI), 180.0f));
+	ASSERT_TRUE(AlmostEqual(Deg(3.0f * PI / 2.0f), 270.0f));
+	ASSERT_TRUE(AlmostEqual(Deg(2.0f * PI), 360.0f));
+	ASSERT_TRUE(AlmostEqual(Deg(-PI / 4.0f), -45.0f));
+}
