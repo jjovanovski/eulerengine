18b4feddb4f89554f6d0f82b06552afbaf0c6411
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2021-01-07 14:22:15 +0100

    Added ClampInt tests

--- src/tests/MathTests.cpp
+++ src/tests/MathTests.cpp
@@ -62,3 +62,14 @@ TEST(MathTests, Clamp) {
 	ASSERT_EQ(Clamp(-2.5f, -2.0f, -1.0f), -2.0f);
 	ASSERT_EQ(Clamp(-0.5f, -2.0f, -1.0f), -1.0f);
 }
+
+TEST(MathTests, ClampInt) {
+	ASSERT_EQ(Clamp(1, 0, 2), 1);
+	ASSERT_EQ(Clamp(-1, 0, 2), 0);
+	ASSERT_EQ(Clamp(3, 0, 2), 2);
+	ASSERT_EQ(Clamp(0, 0, 2), 0);
+	ASSERT_EQ(Clamp(1, 0, 1), 1);
+	ASSERT_EQ(Clamp(-2, -3, -1), -2);
+	ASSERT_EQ(Clamp(-4, -3, -1), -3);
+	ASSERT_EQ(Clamp(-1, -3, -1), -1);
+}
