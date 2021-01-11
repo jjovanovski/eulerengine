c310426fc6439f24594fdccacec1d2e850cae701
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2021-01-11 11:27:51 +0100

    Added model loading code

--- src/core/resources/ModelResource.cpp
+++ src/core/resources/ModelResource.cpp
@@ -1,3 +1,47 @@
 #include "ModelResource.h"
 
+#include <iostream>
+#include <fstream>
+
 using namespace Euler;
+
+void ModelResource::Load(const char* filePath)
+{
+	std::ifstream fs;
+	fs.open(filePath);
+
+	uint32_t vertexCount;
+	uint32_t indexCount;
+
+	fs >> vertexCount;
+	fs >> indexCount;
+
+	Vertices.resize(vertexCount);
+	Indices.resize(indexCount);
+
+	for (uint32_t i = 0; i < vertexCount; i++)
+	{
+		fs >> Vertices[i].Position.x;
+		fs >> Vertices[i].Position.y;
+		fs >> Vertices[i].Position.z;
+
+		fs >> Vertices[i].Normal.x;
+		fs >> Vertices[i].Normal.y;
+		fs >> Vertices[i].Normal.z;
+
+		fs >> Vertices[i].UV.x;
+		fs >> Vertices[i].UV.y;
+	}
+
+	for (uint32_t i = 0; i < indexCount; i++)
+	{
+		fs >> Indices[i];
+	}
+}
+
+void ModelResource::Unload()
+{
+	// trick to force the vectors to free-up the memory
+	std::vector<Vertex>().swap(Vertices);
+	std::vector<uint32_t>().swap(Indices);
+}
--- src/core/resources/ModelResource.h
+++ src/core/resources/ModelResource.h
@@ -1,12 +1,19 @@
 #pragma once
 
 #include "../API.h"
+#include "../graphics/Vertex.h"
+
+#include <vector>
 
 namespace Euler
 {
 	class EULER_API ModelResource
 	{
 	public:
+		std::vector<Vertex> Vertices;
+		std::vector<uint32_t> Indices;
 
+		void Load(const char* filePath);
+		void Unload();
 	};
 }
