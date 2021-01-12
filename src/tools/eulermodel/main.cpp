9e55caa194a5bcd1c8220725fc3b8f77dbe1144b
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2021-01-12 13:07:16 +0100

    Multiple mesh support when writing .bem files

--- src/tools/eulermodel/main.cpp
+++ src/tools/eulermodel/main.cpp
@@ -21,8 +21,10 @@ int main(int argc, char** argv)
 
 	std::cout << "Enter file path: ";
 
-	std::string filePath = "man.obj";
-	//std::cin >> filePath;
+	std::string filePath;
+	std::cin >> filePath;
+
+	std::cout << "Reading file " << filePath << std::endl;
 
 	const aiScene* scene = importer.ReadFile(
 		filePath,
@@ -30,8 +32,8 @@ int main(int argc, char** argv)
 		| aiProcess_MakeLeftHanded
 		| aiProcess_Triangulate
 		| aiProcess_FlipWindingOrder
-		| aiProcess_JoinIdenticalVertices
 		| aiProcess_FlipUVs
+		| aiProcess_JoinIdenticalVertices
 	);
 
 	if (!scene) 
@@ -46,83 +48,87 @@ int main(int argc, char** argv)
 		return 0;
 	}
 
-	// TODO: update this program to support multiple meshes
-	
-	/* === load model using assimp === */
-	
-	Mesh mesh;
+	/* === print file info === */
 
-	aiMesh* x = scene->mMeshes[0];
+	std::cout << "File loaded" << std::endl;
+	std::cout << "Meshes: " << scene->mNumMeshes << std::endl;
 
-	mesh.Vertices.resize(x->mNumVertices);
-	mesh.Indices.resize(x->mNumFaces * 3);
+	/* === convert model to our data structures === */
+	std::vector<Mesh> meshes(scene->mNumMeshes);
 
-	for (int i = 0; i < x->mNumVertices; i++)
+	for (int meshIndex = 0; meshIndex < scene->mNumMeshes; meshIndex++)
 	{
-		auto position = x->mVertices[i];
-		auto normal = x->mNormals[i];
-		auto uv = x->mTextureCoords[0][i];
+		aiMesh* aiMesh = scene->mMeshes[meshIndex];
 
-		mesh.Vertices[i].Position = Vec3(position.x, position.y, position.z);
-		mesh.Vertices[i].Normal = Vec3(normal.x, normal.y, normal.z);
-		mesh.Vertices[i].UV = Vec2(uv.x, uv.y);
+		std::cout << "--- Mesh " << meshIndex << " ---" << std::endl;
+		std::cout << "Vertices: " << aiMesh->mNumVertices << std::endl;
+		std::cout << "Has positions: " << aiMesh->HasPositions() << std::endl;
+		std::cout << "Has normals: " << aiMesh->HasNormals() << std::endl;
+		std::cout << "Has UVs: " << aiMesh->HasTextureCoords(0) << std::endl;
+		std::cout << "Faces (triangles): " << aiMesh->mNumFaces << std::endl;
+		std::cout << "Bones: " << aiMesh->mNumBones << std::endl;
+
+		Mesh* mesh = &meshes[meshIndex];
+
+		mesh->Vertices.resize(aiMesh->mNumVertices);
+		mesh->Indices.resize(aiMesh->mNumFaces * 3);
+
+		// write verts
+		for (int i = 0; i < aiMesh->mNumVertices; i++)
+		{
+			auto position = aiMesh->mVertices[i];
+			mesh->Vertices[i].Position = Vec3(position.x, position.y, position.z);
+
+			if (aiMesh->mNormals != nullptr)
+			{
+				auto normal = aiMesh->mNormals[i];
+				mesh->Vertices[i].Normal = Vec3(normal.x, normal.y, normal.z);
+			}
+
+			if (aiMesh->mTextureCoords != nullptr && aiMesh->mTextureCoords[0] != nullptr)
+			{
+				auto uv = aiMesh->mTextureCoords[0][i];
+				mesh->Vertices[i].UV = Vec2(uv.x, uv.y);
+			}
+		}
+
+		// write indices
+		for (int i = 0; i < aiMesh->mNumFaces * 3; i += 3)
+		{
+			auto face = aiMesh->mFaces[i / 3];
+
+			mesh->Indices[i + 0] = face.mIndices[0];
+			mesh->Indices[i + 1] = face.mIndices[1];
+			mesh->Indices[i + 2] = face.mIndices[2];
+		}
 	}
-
-	for (int i = 0; i < x->mNumFaces * 3; i += 3)
-	{
-		auto face = x->mFaces[i/3];
-
-		mesh.Indices[i + 0] = face.mIndices[0];
-		mesh.Indices[i + 1] = face.mIndices[1];
-		mesh.Indices[i + 2] = face.mIndices[2];
-	}
-
-	/* === write the .eulermodel file === */
-
-	//std::ofstream fs("man.eulermodel");
-
-	//uint32_t vertexCount = mesh.Vertices.size();
-	//uint32_t indexCount = mesh.Indices.size();
-	//fs << vertexCount << " ";
-	//fs << indexCount << " ";
-
-	//std::cout << "Writing... (vertices: " << vertexCount << ", indices: " << indexCount << ")" << std::endl;
-
-	//for (int i = 0; i < mesh.Vertices.size(); i++)
-	//{
-	//	fs << mesh.Vertices[i].Position.x << " ";
-	//	fs << mesh.Vertices[i].Position.y << " ";
-	//	fs << mesh.Vertices[i].Position.z << " ";
-
-	//	fs << mesh.Vertices[i].Normal.x << " ";
-	//	fs << mesh.Vertices[i].Normal.y << " ";
-	//	fs << mesh.Vertices[i].Normal.z << " ";
-
-	//	fs << mesh.Vertices[i].UV.x << " ";
-	//	fs << mesh.Vertices[i].UV.y << " ";
-	//}
-
-	//for (int i = 0; i < mesh.Indices.size(); i++)
-	//{
-	//	fs << mesh.Indices[i] << " ";
-	//}
-
-	//fs.close();
 
 	/* === write the .bem (binary euler model) file === */
 
-	std::ofstream bfs("man.bem", std::ios::out | std::ios::binary);
 
-	uint32_t bvertexCount = mesh.Vertices.size();
-	uint32_t bindexCount = mesh.Indices.size();
+	std::string fileName = filePath.substr(0, filePath.length() - filePath.find_last_of(".") + 1) + ".bem";
+	std::cout << "Writing output file " << fileName << std::endl;
 
-	bfs.write((const char*)(&bvertexCount), sizeof(bvertexCount));
-	bfs.write((const char*)(&bindexCount), sizeof(bindexCount));
+	std::ofstream bfs(fileName, std::ios::out | std::ios::binary);
 
-	std::cout << "Writing binary... (vertices: " << bvertexCount << ", indices: " << bindexCount << ")" << std::endl;
+	uint32_t numberOfMeshes = meshes.size();
+	bfs.write((const char*)(&numberOfMeshes), sizeof(numberOfMeshes));
 
-	bfs.write((const char*)mesh.Vertices.data(), mesh.Vertices.size() * sizeof(Euler::Vertex));
-	bfs.write((const char*)mesh.Indices.data(), mesh.Indices.size() * sizeof(uint32_t));
+	for (int meshIndex = 0; meshIndex < numberOfMeshes; meshIndex++)
+	{
+		Mesh* mesh = &meshes[meshIndex];
+
+		uint32_t vertexCount = mesh->Vertices.size();
+		uint32_t indexCount = mesh->Indices.size();
+
+		bfs.write((const char*)(&vertexCount), sizeof(vertexCount));
+		bfs.write((const char*)(&indexCount), sizeof(indexCount));
+
+		bfs.write((const char*)mesh->Vertices.data(), mesh->Vertices.size() * sizeof(Euler::Vertex));
+		bfs.write((const char*)mesh->Indices.data(), mesh->Indices.size() * sizeof(uint32_t));
+	}
 
 	bfs.close();
+
+	std::cout << "Completed" << std::endl;
 }
