5e97bb3e108b1a2ead0a3cd7e34d5ad9d61b1ddb
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2021-02-12 15:36:13 +0100

    Some shadow app setup

--- bin/Shadows/Debug/shaders/shader.vert
+++ bin/Shadows/Debug/shaders/shader.vert
@@ -10,27 +10,16 @@ layout(binding = 0, set = 1) uniform Model {
 	mat4 model;
 } model;
 
-layout(binding = 0, set = 4) uniform BoneTransforms {
-	mat4 m[32];
-} boneTransforms;
-
 layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 normal;
 layout(location = 2) in vec2 uv;
-layout(location = 3) in ivec3 boneIds;
-layout(location = 4) in vec3 boneWeights;
 
 layout(location = 0) out vec3 fragNormal;
 layout(location = 1) out vec2 fragUv;
 layout(location = 2) out vec3 fragPos;
 
 void main() {
-	vec4 bonePosition1 = boneTransforms.m[max(0, boneIds[0])] * vec4(position, 1.0);
-	vec4 bonePosition2 = boneTransforms.m[max(0, boneIds[1])] * vec4(position, 1.0);
-	vec4 bonePosition3 = boneTransforms.m[max(0, boneIds[2])] * vec4(position, 1.0);
-	vec4 bonePosition = bonePosition1 * boneWeights[0] + bonePosition2 * boneWeights[1] + bonePosition3 * boneWeights[2];
-	
-    gl_Position = viewProj.proj * viewProj.view * model.model * bonePosition;
+    gl_Position = viewProj.proj * viewProj.view * model.model * vec4(position.x, position.y, position.z, 1.0);
     fragNormal = mat3(transpose(inverse(model.model))) * normal;
 	fragUv = uv;
 	fragPos = vec3(model.model * vec4(position.x, -position.y, position.z, 1.0));
--- src/apps/shadows/main.cpp
+++ src/apps/shadows/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 #include "App.h"
-#include "graphics/AnimatedMesh.h"
-#include "graphics/AnimatedModelPipeline.h"
+#include "graphics/Mesh.h"
+#include "graphics/ModelPipeline.h"
 #include "graphics/Camera.h"
 #include "graphics/Vertex.h"
 #include "graphics/Texture.h"
@@ -10,7 +10,7 @@
 #include "graphics/Animator.h"
 #include "math/Math.h"
 #include "resources/TextureResource.h"
-#include "resources/AnimatedModelResource.h"
+#include "resources/ModelResource.h"
 
 #include "stb_image.h"
 
@@ -20,17 +20,21 @@ using namespace Euler;
 
 class SandboxApp : public App
 {
-private:
-	Graphics::AnimatedModelPipeline _modelPipeline;
+private:	
+	Graphics::ModelPipeline _modelPipeline;
 	Graphics::DirectionalLight _dirLight;
 	Camera _camera;
-	Graphics::Texture _texture;
-	AnimatedMesh _mesh;
-	Graphics::MeshMaterial _meshMaterial;
-	AnimatedModel _model;
-	AnimatedModelResource _modelResource;
-	Animator _animator;
 	
+	ModelResource _cubeModelRes;
+	ModelResource _planeModelRes;
+
+	Graphics::Texture _woodTexture;
+	
+	Mesh _cubeMesh;
+	Graphics::MeshMaterial _cubeMeshMat;
+
+	Model _cubeModel;
+	Model _floorModel;
 
 public:
 	void OnCreate() override
@@ -40,65 +44,64 @@ public:
 		// setup light
 		_dirLight.Direction = Vec3(1, 1, 1);
 		_dirLight.Color = Vec3(1, 1, 1);
-		_dirLight.Intensity = 0.5f;
+		_dirLight.Intensity = 1.0f;
 		_modelPipeline.DirLight = &_dirLight;
 		_modelPipeline.AmbLight.Color = Vec3(1, 1, 1);
-		_modelPipeline.AmbLight.Intensity = 0.5f;
+		_modelPipeline.AmbLight.Intensity = 0.1f;
 
 		// create camera
 		_camera.Init(1920, 1080, 60.0f, 0.01f, 100.0f);
 		_camera.Transform.SetPosition(Vec3(0, 0, 2));
 		_camera.Transform.SetRotation(Quaternion::Euler(Math::Rad(180.0f), Vec3(0, 1, 0)));
 
+		// load models
+		_cubeModelRes.Load("res/cube.bem");
+		_planeModelRes.Load("res/plane.bem");
+
 		// load texture
 		TextureResource textureResource;
-		textureResource.Load("diffuse.png", TEXTURE_CHANNELS_RGBA);
-
-		_texture.Shininess = 2.0f;
-		_texture.Create(Vulkan, &textureResource, _modelPipeline.MaterialLayout);
-
+		textureResource.Load("res/wood.jpg", TEXTURE_CHANNELS_RGBA);
+		_woodTexture.Shininess = 2.0f;
+		_woodTexture.Create(Vulkan, &textureResource, _modelPipeline.MaterialLayout);
 		textureResource.Unload();
 
-		// create mesh
-		_modelResource.Load("model.beam");
-
-		_mesh.Vertices = _modelResource.Vertices;
-		_mesh.Indices = _modelResource.Indices;
-		_mesh.Texture = &_texture;
-		_mesh.Create(Vulkan);
-
 		// create mesh material
-		_meshMaterial.AnimatedMesh = &_mesh;
-		_meshMaterial.Texture = &_texture;
+		_cubeMesh.Vertices = _cubeModelRes.Vertices;
+		_cubeMesh.Indices = _cubeModelRes.Indices;
+		_cubeMesh.Create(Vulkan);
 
-		// create model
-		_model.Transform.SetPosition(Vec3(0, -0.5f, 1));
-		_model.Transform.SetScale(0.1f);
-		_model.Transform.SetRotation(Quaternion::Euler(1.15f * PI, Vec3(0, 1, 0)) * Quaternion::Euler(PI / 2.0f, Vec3(1, 0, 0)));
-		_model.Drawables.push_back(&_meshMaterial);
-		_modelPipeline.Models.push_back(&_model);
+		_cubeMeshMat.Mesh = &_cubeMesh;
+		_cubeMeshMat.Texture = &_woodTexture;
 
-		_animator.Animation = _modelResource.Animations[0];
-		_animator.BoneParents = _modelResource.BoneParents;
-		_animator.BoneOffsetMatrices = _modelResource.BoneOffsetMatrices;
-		_animator.Start();
+		_cubeModel.Transform.SetPosition(Vec3(0, -0.5f, 0));
+		_cubeModel.Transform.SetScale(0.1f);
+		_cubeModel.Transform.SetRotation(Quaternion::Euler(Math::Rad(45.0f), Vec3(0, 1, 1)));
+		_cubeModel.Drawables.push_back(&_cubeMeshMat);
+		_modelPipeline.Models.push_back(&_cubeModel);
+		
+		_floorModel.Transform.SetPosition(Vec3(0, -1, 0));
+		_floorModel.Transform.SetScale(10.0f, 0.1f, 10.0f);
+		_floorModel.Drawables.push_back(&_cubeMeshMat);
+		_modelPipeline.Models.push_back(&_floorModel);
 	}
 
 	void OnUpdate() override
 	{
-
 	}
 
 	void OnDraw() override
 	{
-		_animator.Update();
-		_modelPipeline.RecordCommands(_camera.GetViewProj(), _animator.BoneMatrices);
+		_modelPipeline.RecordCommands(_camera.GetViewProj());
 	}
 
 	void OnDestroy() override
 	{
-		_mesh.Destroy(Vulkan);
-		_modelResource.Unload();
+		_cubeModelRes.Unload();
+		_planeModelRes.Unload();
+
+		_cubeMesh.Destroy(Vulkan);
+		_woodTexture.Destroy();
+
 		_modelPipeline.Destroy();
 	}
 };
