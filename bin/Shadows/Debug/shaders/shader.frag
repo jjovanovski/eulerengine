23a68580db9dbade91770404caf3217a8d79f131
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2021-02-12 14:49:13 +0100

    Added another app for shadows

---
+++ bin/Shadows/Debug/shaders/shader.frag
@@ -0,0 +1,40 @@
+#version 450
+#extension GL_ARB_separate_shader_objects : enable
+
+layout(location = 0) in vec3 fragNormal;
+layout(location = 1) in vec2 fragUv;
+layout(location = 2) in vec3 fragPos;
+
+layout(location = 0) out vec4 outColor;
+
+layout(binding = 0, set = 2) uniform sampler2D tex;
+layout(binding = 1, set = 2) uniform Material {
+	float shininess;
+} material;
+
+layout(binding = 0, set = 3) uniform DirectionalLight {
+	vec3 direction;
+	vec3 color;
+	float intensity;
+} directionalLight;
+
+layout(binding = 1, set = 3) uniform AmbientLight {
+	vec3 cameraPosition;
+	vec3 color;
+	float intensity;
+} ambientLight;
+
+
+void main() {
+	vec3 ambLight = ambientLight.color * ambientLight.intensity;
+	
+	vec3 dirLight = directionalLight.color * max(0, dot(directionalLight.direction, normalize(fragNormal))) * directionalLight.intensity;
+	
+	// specular
+    vec3 viewDir = normalize(ambientLight.cameraPosition - fragPos);
+    vec3 reflectDir = reflect(directionalLight.direction, fragNormal);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = directionalLight.color * spec * 0.5; 
+	
+    outColor = vec4(texture(tex, fragUv).xyz * (dirLight + ambLight), 1);
+}
---
+++ src/apps/shadows/main.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+
+#include "App.h"
+#include "graphics/AnimatedMesh.h"
+#include "graphics/AnimatedModelPipeline.h"
+#include "graphics/Camera.h"
+#include "graphics/Vertex.h"
+#include "graphics/Texture.h"
+#include "graphics/DirectionalLight.h"
+#include "graphics/Animator.h"
+#include "math/Math.h"
+#include "resources/TextureResource.h"
+#include "resources/AnimatedModelResource.h"
+
+#include "stb_image.h"
+
+#include <vector>
+
+using namespace Euler;
+
+class SandboxApp : public App
+{
+private:
+	Graphics::AnimatedModelPipeline _modelPipeline;
+	Graphics::DirectionalLight _dirLight;
+	Camera _camera;
+	Graphics::Texture _texture;
+	AnimatedMesh _mesh;
+	Graphics::MeshMaterial _meshMaterial;
+	AnimatedModel _model;
+	AnimatedModelResource _modelResource;
+	Animator _animator;
+	
+
+public:
+	void OnCreate() override
+	{
+		_modelPipeline.Create(Vulkan, 1920, 1080);
+
+		// setup light
+		_dirLight.Direction = Vec3(1, 1, 1);
+		_dirLight.Color = Vec3(1, 1, 1);
+		_dirLight.Intensity = 0.5f;
+		_modelPipeline.DirLight = &_dirLight;
+		_modelPipeline.AmbLight.Color = Vec3(1, 1, 1);
+		_modelPipeline.AmbLight.Intensity = 0.5f;
+
+		// create camera
+		_camera.Init(1920, 1080, 60.0f, 0.01f, 100.0f);
+		_camera.Transform.SetPosition(Vec3(0, 0, 2));
+		_camera.Transform.SetRotation(Quaternion::Euler(Math::Rad(180.0f), Vec3(0, 1, 0)));
+
+		// load texture
+		TextureResource textureResource;
+		textureResource.Load("diffuse.png", TEXTURE_CHANNELS_RGBA);
+
+		_texture.Shininess = 2.0f;
+		_texture.Create(Vulkan, &textureResource, _modelPipeline.MaterialLayout);
+
+		textureResource.Unload();
+
+		// create mesh
+		_modelResource.Load("model.beam");
+
+		_mesh.Vertices = _modelResource.Vertices;
+		_mesh.Indices = _modelResource.Indices;
+		_mesh.Texture = &_texture;
+		_mesh.Create(Vulkan);
+
+		// create mesh material
+		_meshMaterial.AnimatedMesh = &_mesh;
+		_meshMaterial.Texture = &_texture;
+
+		// create model
+		_model.Transform.SetPosition(Vec3(0, -0.5f, 1));
+		_model.Transform.SetScale(0.1f);
+		_model.Transform.SetRotation(Quaternion::Euler(1.15f * PI, Vec3(0, 1, 0)) * Quaternion::Euler(PI / 2.0f, Vec3(1, 0, 0)));
+		_model.Drawables.push_back(&_meshMaterial);
+		_modelPipeline.Models.push_back(&_model);
+
+		_animator.Animation = _modelResource.Animations[0];
+		_animator.BoneParents = _modelResource.BoneParents;
+		_animator.BoneOffsetMatrices = _modelResource.BoneOffsetMatrices;
+		_animator.Start();
+	}
+
+	void OnUpdate() override
+	{
+
+	}
+
+	void OnDraw() override
+	{
+		_animator.Update();
+		_modelPipeline.RecordCommands(_camera.GetViewProj(), _animator.BoneMatrices);
+	}
+
+	void OnDestroy() override
+	{
+		_mesh.Destroy(Vulkan);
+		_modelResource.Unload();
+		_modelPipeline.Destroy();
+	}
+};
+
+int main(int argc, char** argv)
+{	
+	SandboxApp app;
+	app.Run();
+
+	return 0;
+}
